34c04424d436e49a2ac64caa324c889847735fad
strd3983 <strd3983@gmail.com>
2021-07-20 14:15:34 +0900

    v20210720 - cad ex03

--- cad/LED_ex03/LED_ex03my.ino
+++ cad/LED_ex03/LED_ex03my.ino
@@ -2,35 +2,34 @@
 #define PSWpin 2
 #define varRpin A0
 volatile int n = 0; //割り込み回数
-volatile int PSW_old = HIGH;
 
 void setup()
 {
   pinMode(LEDpin, OUTPUT);
   pinMode(PSWpin, INPUT);
   pinMode(varRpin, OUTPUT);
+  //プッシュスイッチが押されたとき割り込み
+  attachInterrupt(0, ledISR, CHANGE);
   Serial.begin(9600);
 }
 
 void ledISR()
 {
-  int PSW = digitalRead(PSWpin);
+  //変化したときに押しているときは反応しない
+  if (digitalRead(PSWpin) == HIGH)
+    return;
+  //変化したときに話した場合
+  n++;
   if (n % 2 == 0)
     digitalWrite(LEDpin, LOW);
   else
     digitalWrite(LEDpin, HIGH);
-  PSW_old = PSW;
 }
 
 void loop()
 {
-  int PSW = digitalRead(PSWpin);
-
-    attachInterrupt(0, ledISR, HIGH);
-  //プッシュスイッチが押されたとき割り込み
-  double varR = analogRead(varRpin);
-  Serial.print(varR);
+  Serial.print(analogRead(varRpin));
   Serial.print("\t");
   Serial.println(n);
-  delay(2500);
+  delay(10000);
 }
--- cad/WRITE/WRITE.ino
+++ cad/WRITE/WRITE.ino
@@ -9,12 +9,15 @@ void setup()
   pinMode(PSWpin, INPUT);
   pinMode(varRpin, OUTPUT);
   //プッシュスイッチが押されたとき割り込み
-  attachInterrupt(0, ledISR, FALLING);
+  attachInterrupt(0, ledISR, CHANGE);
   Serial.begin(9600);
 }
 
 void ledISR()
 {
+  //変化したときに押しているときは反応しない
+  if (digitalRead(PSWpin) == LOW)
+    return;
   n++;
   if (n % 2 == 0)
     digitalWrite(LEDpin, LOW);
